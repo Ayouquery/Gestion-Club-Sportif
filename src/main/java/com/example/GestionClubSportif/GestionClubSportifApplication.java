a62ebf9769d3c99f2be2b7f77b0b1b78fc287b50
EZZOUITINE <ayoub.ezzouitine@capgemini.com>
2018-11-14 12:05:29 +0100

    Configuration de base pour Spring Boot

---
+++ src/main/java/com/example/GestionClubSportif/GestionClubSportifApplication.java
@@ -0,0 +1,12 @@
+package com.example.GestionClubSportif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionClubSportifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionClubSportifApplication.class, args);
+	}
+}
